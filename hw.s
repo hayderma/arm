45ee9407ccca70443acbd299a325f15853eb6a0b
hayderma <hayder.alhassany@mavs.uta.edu>
2016-04-06 13:06:30 -0500

    Update hw.s

--- hw.s
+++ hw.s
@@ -11,16 +11,16 @@
 main:
     MOV R4,#3		@ LOOP_COUNTER (3 INPUTS)
     loop_label:  	
-    bl      scanf
+    BL      _scanf
     sub     r4, r4, #1  
     cmp     r4, #0  
     bne     loop_label    @ for (i = 3; i != 0; i--)
-    MOV R1,#5
-    MOV R2,#10
+   @ MOV R1,#5
+    @MOV R2,#10
     LDR R0,=print_str   @ store string address in R0
     
      
-    BL  _scanf              @ branch to scanf procedure with return
+   @ BL  _scanf              @ branch to scanf procedure with return
     MOV R1, R0              @ move return value R0 to argument register R1
   
     
@@ -38,12 +38,6 @@ _scanf:
     MOV PC, R4              @ return
 
 
-    
-    
-    
-    
-    
-    
     BL   _add
     BL printf           @ call printf
     B   _exit           @ branch to exit procedure with no return
@@ -58,7 +52,7 @@ _multiply:
 	mul R1,R1,R2     @multiplying
 	MOV PC,LR
 _M:
-	cmp R1,R2
+	CMP R1,R2
 	MOVGT R1,R2      @if R2 > R1 , move R2 to R1 for printing
 	
 
