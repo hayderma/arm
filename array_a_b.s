b3f3124b9707b0ddae7977fadc63992b95f8a6ae
hayderma <hayder.alhassany@mavs.uta.edu>
2016-05-01 20:42:12 -0500

    Update array_a_b.s

--- array_a_b.s
+++ array_a_b.s
@@ -80,8 +80,10 @@ _sort_ascending:           @ function to read from array a, compare a[i] with a[
     PUSH {R0}               @ backup register before printf
     PUSH {R2}               @ backup register before printf (address of b[i])
     PUSH {R1}               @ backup register before printf (value of b[i])
-    MOV R2, R9              @ move array value to R2 for printf
-    MOV R1, R8              @ move array index to R1 for printf
+    MOV R2, #1              @ move array value to R2 for printf
+    MOV R1, #2              @ move array index to R1 for printf
+    MOV R3,#3
+    MOV R4,#4
     BL  _printf_b           @ branch to print procedure with return
     POP {R1}                @ restore register
     POP {R2}                @ restore register
