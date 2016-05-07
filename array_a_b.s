41947ae039c5b538939161f265030c2e8288cd63
hayderma <hayder.alhassany@mavs.uta.edu>
2016-05-07 14:00:37 -0500

    Update array_a_b.s

--- array_a_b.s
+++ array_a_b.s
@@ -21,7 +21,8 @@ writeloop:
     LSL R2, R3, #2          @ multiply index*4 to get array offset
     ADD R2, R1, R2          @ R2 now has the element address
     BL _scanf
-    STR R3, [R2]            @ write the address of a[i] to a[i]
+    MOV R4,R0
+    STR R4, [R2]            @ write input to a[i]
     ADD R3, R3, #1          @ increment index
     B   writeloop           @ branch to next loop iteration
 writedone:
