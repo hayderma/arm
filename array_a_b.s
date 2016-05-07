724fc68d873e0b9cd742810e2855e646be8f76bd
hayderma <hayder.alhassany@mavs.uta.edu>
2016-05-07 13:49:56 -0500

    Update array_a_b.s

--- array_a_b.s
+++ array_a_b.s
@@ -47,39 +47,7 @@ readloop:
 readdone:
     MOV R3,#0               @reset counter (i)
     
-_sort_ascending:           @ function to read from array a, compare a[i] with a[i+1] and write to b[i]
-    MOV R12,#0             @R12 is (i) for array b
-    CMP R0, #10            @ check to see if we are done iterating
-    BEQ _exit              @ exit if done comparing and storing in b
-    LDR R1, =a              @ get address of a
-    LDR R7, =b              @get address of b
-    LSL R2, R0, #2          @ multiply index*4 to get array offset for array A
-    LSL R8, R12, #2          @ multiply index*4 to get array offset for array B
-    ADD R2, R1, R2          @ R2 now has the element address for array A
-    ADD R8, R7, R8          @ R8 now has the element address for array B
-    LDR R1, [R2]            @ read the array at address 
-    MOV R9,R1               @ MOVE a[i] to R9 for comparsion with R10 (a[i+1]) later
-    ADD R0, R0, #1          @ increment index i for array a for next element
-    LSL R2, R0, #2          @ multiply index*4 to get NEXT array offset for array A
-    ADD R2, R1, R2          @ R2 now has the NEXT element address for array A
-    LDR R1, [R2]            @ read the NEXT array element at address in array A
-    MOV R10,R1              @ MOVE a[i+1] to R10 for compasion with R9 (a[i])
-    CMP   R9, R10           @compare i and i+1 
-    MOVLE R11, R9           @if i < i+1 , MOVE i to R11 to write to array b[i]
-    MOVGT R11, R10          @if i > i+1, MOVE i+1 to R11 to write to array b[i]
-    STR R11, [R8]           @ write  smallest value stored in R11 to b[i]
-    PUSH {R0}               @ backup register before printf
-    PUSH {R2}               @ backup register before printf (address of b[i])
-    PUSH {R1}               @ backup register before printf (value of b[i])
-    MOV R2, R11             @ move array value to R2 for printf
-    MOV R1, R8              @ move array index to R1 for printf
-    BL  _printf_b           @ branch to print procedure with return
-    POP {R1}                @ restore register
-    POP {R2}                @ restore register
-    POP {R0}                @ restore register
-    ADD R0, R0, #1          @ increment index i for array a
-    ADD R12,R12,#1          @ increment index i for array b
-    B _sort_ascending       @ branch to next loop iteration
+
     
  
 _scanf:
