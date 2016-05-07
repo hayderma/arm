5dbf73c52f73f2063f5a61ed4bd20ba31db664a2
hayderma <hayder.alhassany@mavs.uta.edu>
2016-05-07 13:52:47 -0500

    Update array_a_b.s

--- array_a_b.s
+++ array_a_b.s
@@ -50,17 +50,7 @@ readdone:
 
     
  
-_scanf:
-    PUSH {LR}               @ store the return address
-    PUSH {R1}               @ backup regsiter value
-    LDR R0, =format_str     @ R0 contains address of format string
-    SUB SP, SP, #4          @ make room on stack
-    MOV R1, SP              @ move SP to R1 to store entry on stack
-    BL scanf                @ call scanf
-    LDR R0, [SP]            @ load value at SP into R0
-    ADD SP, SP, #4          @ remove value from stack
-    POP {R1}                @ restore register value
-    POP {PC}                @ restore the stack pointer and return   
+ 
     
  
 _exit:  
