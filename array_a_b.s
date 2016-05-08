82cc69d7a78a5296dae9ce5350610b893495f75d
hayderma <hayder.alhassany@mavs.uta.edu>
2016-05-07 21:03:57 -0500

    Update array_a_b.s

--- array_a_b.s
+++ array_a_b.s
@@ -40,24 +40,7 @@ readloop:
 readdone:
     MOV R0,#0
 
-_mini:
-    CMP R0, #10            @ check to see if we are done iterating
-    BEQ _exit            @ exit loop if done
-    LDR R1, =a              @ get address of a
-    LSL R2, R0, #2          @ multiply index*4 to get array offset
-    ADD R2, R1, R2          @ R2 now has the element address
-    LDR R1, [R2]            @ read the array at address 
-    PUSH {R0}               @ backup register before printf
-    PUSH {R1}               @ backup register before printf
-    PUSH {R2}               @ backup register before printf
-    MOV R2, R1              @ move array value to R2 for printf
-    MOV R1, R0              @ move array index to R1 for printf
-    BL  _printf             @ branch to print procedure with return
-    POP {R2}                @ restore register
-    POP {R1}                @ restore register
-    POP {R0}                @ restore register
-    ADD R0, R0, #1          @ increment index
-    B   readloop            @ branch to next loop iteration
+
 
 
 _exit:  
