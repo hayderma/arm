bd355baa9006bfc7208aab60507d69ef47bd9fcb
hayderma <hayder.alhassany@mavs.uta.edu>
2016-05-07 19:41:32 -0500

    Update array_a_b.s

--- array_a_b.s
+++ array_a_b.s
@@ -37,7 +37,7 @@ readloop:
     
 readloop_2:
     CMP R0, #10            @ check to see if we are done iterating
-    BEQ readdone            @ exit loop if done
+    BEQ writedone            @ exit loop if done
     LDR R1, =a              @ get address of a
     LSL R2, R0, #2          @ multiply index*4 to get array offset
     ADD R2, R1, R2          @ R2 now has the element address
